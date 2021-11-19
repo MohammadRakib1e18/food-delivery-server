ab5a32e1b281891edb82df69ba52c934391d4e0a
MohammadRakib1e18 <trustedrakib@gmail.com>
2021-11-19 15:22:05 +0600

    added

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.np0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        const database = client.db('onineShop');
-        const serviceCollection = database.collection('services');
+        const database = client.db("onineShop");
+        const serviceCollection = database.collection("services");
 
         //GET Api
-        app.get('/services', async(req, res)=>{
+        app.get("/services", async (req, res) => {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
         const orderCollection = database.collection("order");
 
@@ -34,18 +37,24 @@ async function run(){
             console.log(result);
             res.json({ result });
         });
-
-    }
-    finally{
+        app.get("/order", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            console.log(query);
+            const cursor = orderCollection.find(query);
+            const getOrders = await cursor.toArray();
+            res.send(getOrders);
+        });
+    } finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('Server is running');
-})
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
 
-app.listen(port, ()=>{
-    console.log('Server is running at port', port);
-})
+app.listen(port, () => {
+    console.log("Server is running at port", port);
+});
